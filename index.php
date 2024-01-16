a83cc8db32687dea4f5253a006a5932f1a7dde6a
unknown <mdsakibhr7@gmail.com>
2024-01-16 14:08:27 +0600

    start php code

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$array = array('myName' => 'Becca', 'favColor' => 'violet', 'favMovie' => 'Empire Strikes Back', 'favBook' => 'Lullaby', 'favWeb' => 'twitter.com');
+
+$myName = $array['myName'];
+
+echo "<h1>  $myName </h1>";
