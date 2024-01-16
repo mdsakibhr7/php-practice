55eae643961f56832699332c188c09df09b3610b
unknown <mdsakibhr7@gmail.com>
2024-01-16 14:39:33 +0600

    Remove value from array

--- array.php
+++ array.php
@@ -9,3 +9,18 @@ foreach ($array as $key => $value) {
     echo "</li>";
 }
 echo "</ul>";
+
+$array = array('myName' => 'Becca', 'favColor' => 'violet', 'favMovie' => 'Empire Strikes Back', 'favBook' => 'Lullaby', 'favWeb' => 'twitter.com');
+
+echo "<ul>";
+
+foreach ($array as $key => $value) {
+
+    if ($key != 'favMovie') {
+        echo "<li>";
+        echo $value;
+        echo "</li>";
+    }
+
+}
+echo "</ul>";
